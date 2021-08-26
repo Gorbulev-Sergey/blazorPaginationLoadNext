867715f0c061dfc4469942a05b109e48fac9e48e
Серёжа <gorbulevsv@yandex.ru>
2021-08-26 23:33:43 +0300

    Добавьте файлы проекта.

---
+++ Models/post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorPaginationLoadNext.Models
+{
+    public class post
+    {
+        public int id { get; set; }
+        public DateTime date { get; set; } = DateTime.Now;
+        public string title { get; set; }
+        public string description { get; set; }
+        public string content { get; set; }
+        public string cover_image { get; set; }
+    }
+}
